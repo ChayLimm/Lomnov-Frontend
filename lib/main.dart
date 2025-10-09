4eefc1e4ba69940cb9b04917b169141688217a48
Ritsumeotakiri <lollolrith@gmail.com>
2025-10-09 14:26:11 +0700

    Revert "Add Authentication Frontend"
    This reverts commit 0b6a42a7b8df88fe10ef9ae49d0612622ae9cbfc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,57 @@
-import 'package:app/Presentation/provider/auth_viewmodel.dart';
-import 'package:app/Presentation/views/auth_view.dart';
-import 'package:app/Presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/services.dart';
+import 'Presentation/viewmodels/test/sample_viewmodel.dart';
+import 'Presentation/views/Test/home_view.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
+import 'Presentation/themes/app_theme.dart';
+import 'domain/cores/constants/app_constants.dart';
 
-Future<void> main() async {
-  await dotenv.load(fileName: ".env");
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthViewModel()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _navigator = GlobalKey<NavigatorState>();
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      getPages: [
-        GetPage(name: '/', page: () => const AuthView()),
-        GetPage(name: '/home', page: () => const HomeView()),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SampleViewModel()),
+        // Add more providers here as needed
       ],
+      child: GetMaterialApp(
+        title: AppConstants.appName,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light(),
+        darkTheme: AppTheme.dark(),
+        themeMode: ThemeMode.system, // Follows system theme
+        navigatorKey: _navigator,
+        home: const HomeView(),
+        
+        // Enhanced app configuration
+        defaultTransition: Transition.cupertino,
+        transitionDuration: AppConstants.shortAnimation,
+        
+        // Global error handling
+        builder: (context, child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
+            child: child!,
+          );
+        },
+      ),
     );
   }
 }
